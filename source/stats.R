1af081bf984dd057a11291ef9ce3331d97e26338
fizzoo <vixirfizz@gmail.com>
2018-01-04 16:58:09 +0100

    stats done

--- source/stats.R
+++ source/stats.R
@@ -1,22 +1,12 @@
-install.packages(c("tidyverse", "broom", "magrittr", "ggplot2"))
-install.packages("tidyverse")
+#!/usr/bin/env Rscript
+
+## install.packages("tidyverse")
 
 library("tidyverse")
 library("broom")
 library("magrittr")
 
 data <- read_csv("log")
-
-## The held is 0 everywhere, pointless but included for completeness
-data %>%
-    summarize(min(held), max(held), mean(held))
-
-## View means and sd separately with running particles and no
-data %>%
-    group_by(updating) %>%
-    summarize(mean(update), sd(update),
-              mean(display), sd(display),
-              mean(all), sd(all))
 
 updating <- data %>%
     filter(updating==1) %>%
@@ -25,8 +15,37 @@ nonupdating <- data %>%
     filter(updating==0) %>%
     select(-updating, -held)
 
-## A single measurement without update held
-nonupdating %>%
-    filter(abs(update - median(update)) > sd(update))
-nonupdating %>%
-    summarize(min(update), max(update), mean(update), sd(update))
+testing <- F
+if (testing) {
+    ## The held is 0 everywhere, pointless but included for completeness
+    data %>%
+        summarize(min(held), max(held), mean(held))
+
+    ## View means and sd separately with running particles and no
+    data %>%
+        group_by(updating) %>%
+        summarize(mean(update), sd(update),
+                  mean(display), sd(display),
+                  mean(all), sd(all))
+
+    ## Update is zero everywhere when not updating
+    nonupdating %>%
+        summarize(min(update), max(update), mean(update), sd(update))
+
+    ## A few outliers, notably the first display of program run is way slower
+    data %>%
+        filter(abs(display - median(display)) > sd(display))
+
+    ## Aside from the outliers, no difference in display time
+    ## depending on updating. Hence, we seem to be quite free of
+    ## confounding variables, and can hereafter only look at updating.
+    data %>%
+        filter(!(abs(display - median(display)) > sd(display))) %>%
+        group_by(updating) %>%
+        summarize(mean(display), sd(display))
+}
+
+updating %>%
+    summarize(mean(update), sd(update),
+              mean(display), sd(display))
+
